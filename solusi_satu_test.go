473525a2a63022e9d7b5c7e784e06b8cfe0eee25
Fahriansyah <fachryansyah123@gmail.com>
2019-11-08 23:50:56 +0700

    bikin file testing

---
+++ solusi_satu_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestHitung(test *testing.T) {
+	vocal, consonant := Hitung("omama")
+	if vocal != 2 && consonant != 2 {
+		test.Errorf("Count function was error, want(consonant: %d, vocal: %d) got(consonant: %d, vocal: %d)", 2, 2, consonant, vocal)
+	}
+}
