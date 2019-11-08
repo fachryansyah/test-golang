ad76afd5c82145276744f6d18e4c399d4184919d
Fahriansyah <fachryansyah123@gmail.com>
2019-11-09 00:01:22 +0700

    change func test name

--- solusi_satu_test.go
+++ solusi_satu_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestHitung(test *testing.T) {
+func TestCountWord(test *testing.T) {
 	vocal, consonant := CountWord("omama")
 	if vocal != 2 && consonant != 2 {
 		test.Errorf("Count function was error, want(consonant: %d, vocal: %d) got(consonant: %d, vocal: %d)", 2, 2, consonant, vocal)
