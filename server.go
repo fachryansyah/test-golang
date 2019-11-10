2ff61a9325a01fb8485e0d85ed2c1ecbd5633ae4
Fahriansyah <fachryansyah123@gmail.com>
2019-11-10 19:34:01 +0700

    finish create web service

---
+++ server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	word "test-golang/solutionone"
+)
+
+type Response struct {
+	Status  int
+	Message string
+	Data    interface{}
+}
+
+func main() {
+
+	http.HandleFunc("/solusi-satu", solutionOne)
+
+	fmt.Println("Webservice running on port : 1337")
+	http.ListenAndServe(":1337", nil)
+}
+
+func solutionOne(res http.ResponseWriter, req *http.Request) {
+
+	var response Response
+	// var data interface{}
+	response.Status = 200
+	response.Message = "OKE"
+
+	if req.Method == "POST" {
+		input := req.FormValue("word")
+		vocal, consonant := word.CountWord(input)
+
+		data := map[string]interface{}{
+			"HurufHidup": vocal,
+			"HurufMati":  consonant,
+		}
+		response.Data = data
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(response)
+}
